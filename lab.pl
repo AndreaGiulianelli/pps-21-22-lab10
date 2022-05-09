7ad5ac514534f51f130ae8789ee56412c1ccf786
andrea.giulianelli <andrygiulio@gmail.com>
2022-05-09 11:17:33 +0200

    added comment in part1

--- lab.pl
+++ lab.pl
@@ -8,7 +8,10 @@ search2(X, [_|Xs]) :- search2(X, Xs).
 
 % 1.3
 % search_two(Elem, List)
-%  looks for two occurrences of Elem with any element in between
+% looks for two occurrences of Elem with any element in between
+% fully relational 
+%   -> search_two(X, [a, d, a, c, d, c, f, g])
+%   -> search_two(c, [a, _ , _, c, d, _, f, g])
 search_two(X, [X, _, X|_]).
 search_two(X, [_|T]) :- search_two(X, T).
 
@@ -17,4 +20,8 @@ search_two(X, [_|T]) :- search_two(X, T).
 % looks for any Elem that occurs two times , anywhere
 search_anytwo(X, [X|T]) :- search(X, T).
 search_anytwo(X, [H|T]) :- search_anytwo(X, T).
- 
+
+%2.1
+% size ( List , Size )
+% Size will contain the number of elements in List
+
